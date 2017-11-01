16c002b7ad476d03b2f166034fd43ca7ec18eb65
mrojas <mrojas@ultrabook-i5>
2017-11-01 18:34:17 -0500

    version 1 de noviembre 2017 linea 3

--- ejemplogithub/src/ejemplogithub/Ejemplogithub.java
+++ ejemplogithub/src/ejemplogithub/Ejemplogithub.java
@@ -8,7 +8,7 @@ public class Ejemplogithub {
     public static void main(String[] args) {
         System.out.println("linea 1");
         System.out.println("linea 2");
-        System.out.println("linea 3"):
+        System.out.println("linea 3");
     }
     
 }
